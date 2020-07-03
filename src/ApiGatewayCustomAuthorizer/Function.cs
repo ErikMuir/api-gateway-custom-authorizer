6316f496d4d828423d9f1113b4416072e5ca6590
Erik Muir <erikdmuir@gmail.com>
2020-07-03 14:46:30 -0500

    implement policy builder in function handler

--- src/ApiGatewayCustomAuthorizer/Function.cs
+++ src/ApiGatewayCustomAuthorizer/Function.cs
@@ -8,9 +8,49 @@ namespace ApiGatewayCustomAuthorizer
 {
     public class Function
     {
+        private static readonly Logger _logger = Logger.Create<Function>();
+        private readonly IEnvironmentWrapper _env;
+        private readonly IPolicyBuilder _policyBuilder;
+
+        public Function() : this(EnvironmentWrapper.Instance, new PolicyBuilder())
+        { }
+
+        public Function(IEnvironmentWrapper env, IPolicyBuilder policyBuilder)
+        {
+            _env = env;
+            _policyBuilder = policyBuilder;
+        }
+
         public Response FunctionHandler(Request request, ILambdaContext context)
         {
-            throw new NotImplementedException();
+            _env.Request = request;
+            _env.Context = context;
+
+            var isAuthorized = false; // TODO : implement token authorization
+            var apiGatewayArn = ApiGatewayArn.Parse(request.MethodArn);
+            var principalId = null as string; // TODO : get this during token validation process
+
+            if (isAuthorized)
+            {
+                // you could implement claims-based authorization for each resource/method if desired
+                // but this implementation is more of an all-or-nothing approach
+                _policyBuilder.AllowAllMethods();
+            }
+            else
+            {
+                // you could throw an unauthorized exception here if desired
+                // but this implementation returns a policy that denies all resources and methods
+                _policyBuilder.DenyAllMethods();
+            }
+
+            var response = _policyBuilder.Build(apiGatewayArn, principalId);
+
+            // you can add key-value pairs that can be accessed in API Gateway via $context.authorizer.<key>
+            // response.Context.Add("key", "value");
+
+            _logger.LogInformation("Authorizer Response", new { response });
+
+            return response;
         }
     }
 }
--- test/ApiGatewayCustomAuthorizer.Tests/FunctionTests.cs
+++ test/ApiGatewayCustomAuthorizer.Tests/FunctionTests.cs
@@ -7,20 +7,58 @@ namespace ApiGatewayCustomAuthorizer.Tests
 {
     public class FunctionTests
     {
+        private readonly IEnvironmentWrapper _env;
+        private readonly Mock<IPolicyBuilder> _policyBuilder;
         private readonly Function _testObject;
 
         public FunctionTests()
         {
-            _testObject = new Function();
+            _env = EnvironmentWrapper.Instance;
+            _policyBuilder = new Mock<IPolicyBuilder>();
+            _testObject = new Function(_env, _policyBuilder.Object);
         }
 
         [Fact]
-        public void FunctionHandler_Throws()
+        public void FunctionHandler_EnvironmentWrapper_SetsRequest()
         {
-            var exception = Record.Exception(() => _testObject.FunctionHandler(It.IsAny<Request>(), new TestLambdaContext()));
+            var request = new Request();
 
-            Assert.NotNull(exception);
-            Assert.IsType<NotImplementedException>(exception);
+            Record.Exception(() => _testObject.FunctionHandler(request, new TestLambdaContext()));
+
+            Assert.Same(request, _env.Request);
+        }
+
+        [Fact]
+        public void FunctionHandler_EnvironmentWrapper_SetsContext()
+        {
+            var context = new TestLambdaContext();
+
+            Record.Exception(() => _testObject.FunctionHandler(new Request(), context));
+
+            Assert.Same(context, _env.Context);
+        }
+
+        [Fact]
+        public void FunctionHandler_Calls_PolicyBuilder_DenyAllMethods()
+        {
+            var request = new Request { MethodArn = "arn:partition:service:region:awsAccountId:restApiId/stage/verb" };
+            
+            _testObject.FunctionHandler(request, new TestLambdaContext());
+
+            _policyBuilder.Verify(x => x.DenyAllMethods(), Times.Once);
+            _policyBuilder.Verify(x => x.AllowAllMethods(), Times.Never);
+        }
+
+        [Fact]
+        public void FunctionHandler_Returns()
+        {
+            var request = new Request { MethodArn = "arn:partition:service:region:awsAccountId:restApiId/stage/verb" };
+            var response = new Response();
+            _policyBuilder.Setup(x => x.Build(It.IsAny<ApiGatewayArn>(), It.IsAny<string>())).Returns(response);
+
+            var actual = _testObject.FunctionHandler(request, new TestLambdaContext());
+
+            Assert.Equal(response, actual);
         }
     }
 }
