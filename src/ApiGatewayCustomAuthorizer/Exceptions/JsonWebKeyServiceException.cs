467415d7cee0367e6aa132aeba6becf33cab5e31
Erik Muir <erikdmuir@gmail.com>
2020-07-04 12:38:27 -0500

    add JsonWebKeyServiceException

---
+++ src/ApiGatewayCustomAuthorizer/Exceptions/JsonWebKeyServiceException.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ApiGatewayCustomAuthorizer
+{
+    public class JsonWebKeyServiceException : BaseException
+    {
+        public JsonWebKeyServiceException() { }
+        public JsonWebKeyServiceException(string message) : base(message) { }
+        public JsonWebKeyServiceException(Exception inner) : base(nameof(AuthPolicyBuilderException), inner) { }
+        public JsonWebKeyServiceException(string message, Exception inner) : base(message, inner) { }
+    }
+}
