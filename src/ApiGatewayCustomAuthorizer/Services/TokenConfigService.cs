acbf3024b6f2d73751995f7cca2bced0af4e2acd
Erik Muir <erikdmuir@gmail.com>
2020-07-04 12:42:30 -0500

    implement json web key service

--- src/ApiGatewayCustomAuthorizer/Services/TokenConfigService.cs
+++ src/ApiGatewayCustomAuthorizer/Services/TokenConfigService.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using Microsoft.IdentityModel.Tokens;
 
 namespace ApiGatewayCustomAuthorizer
@@ -11,12 +13,14 @@ namespace ApiGatewayCustomAuthorizer
     {
         private static readonly Logger _logger = Logger.Create<TokenConfigService>();
         private readonly IEnvironmentWrapper _env;
+        private readonly IJsonWebKeyService _jwkService;
 
-        public TokenConfigService() : this(EnvironmentWrapper.Instance) { }
+        public TokenConfigService() : this(EnvironmentWrapper.Instance, new JsonWebKeyService()) { }
 
-        public TokenConfigService(IEnvironmentWrapper env)
+        public TokenConfigService(IEnvironmentWrapper env, IJsonWebKeyService jwkService)
         {
             _env = env;
+            _jwkService = jwkService;
         }
 
         public TokenValidationParameters GetJwtConfig()
@@ -27,12 +31,35 @@ namespace ApiGatewayCustomAuthorizer
             {
                 ValidIssuer = _env.Issuer,
                 ValidAudience = _env.Audience,
-                IssuerSigningKeys = null, // TODO : get signing keys
+                IssuerSigningKeys = GetSigningKeys(),
             };
 
             _logger.LogTrace("Successfully built jwt config", new { jwtConfig });
 
             return jwtConfig;
+        }
+
+        public ICollection<SecurityKey> GetSigningKeys()
+        {
+            // It is strongly recommended to store your jwks in an environment variable
+            // to bypass the HttpClient dependency. You would just need to remember to
+            // update the value whenever you cycle the keys.
+            var jwks = _env.Jwks;
+
+            try
+            {
+                _logger.LogTrace("Retrieving signing keys", new { jwks });
+
+                var signingKeys = _jwkService.GetSigningKeys(jwks);
+
+                _logger.LogTrace("Successfuly retrieved signing keys from json", new { signingKeys });
+
+                return signingKeys;
+            }
+            catch (Exception ex)
+            {
+                throw new JsonWebKeyServiceException($"Could not deserialize the signing keys: {jwks}", ex);
+            }
         }
     }
 }
--- test/ApiGatewayCustomAuthorizer.Tests/Services/TokenConfigServiceTests.cs
+++ test/ApiGatewayCustomAuthorizer.Tests/Services/TokenConfigServiceTests.cs
@@ -9,12 +9,14 @@ namespace ApiGatewayCustomAuthorizer.Tests
     {
         private TokenConfigService _testObject;
         private Mock<IEnvironmentWrapper> _env;
+        private Mock<IJsonWebKeyService> _jwkService;
 
         public TokenConfigServiceTests()
         {
             _env = new Mock<IEnvironmentWrapper>();
+            _jwkService = new Mock<IJsonWebKeyService>();
 
-            _testObject = new TokenConfigService(_env.Object);
+            _testObject = new TokenConfigService(_env.Object, _jwkService.Object);
         }
 
         [Fact]
@@ -22,15 +24,27 @@ namespace ApiGatewayCustomAuthorizer.Tests
         {
             var issuer = "fake-issuer";
             var audience = "fake-audience";
-            var expectedKeys = null as List<SecurityKey>;
+            var expectedKeys = new List<SecurityKey>();
             _env.Setup(e => e.Issuer).Returns(issuer);
             _env.Setup(e => e.Audience).Returns(audience);
+            _jwkService.Setup(x => x.GetSigningKeys(It.IsAny<string>())).Returns(expectedKeys);
 
             var actual = _testObject.GetJwtConfig();
 
             Assert.Equal(issuer, actual.ValidIssuer);
             Assert.Equal(audience, actual.ValidAudience);
             Assert.Equal(expectedKeys, actual.IssuerSigningKeys);
+        }
+
+        [Fact]
+        public void GetJwtConfig_JsonWebKeyService_GetSigningKeys()
+        {
+            var jwks = "{\"keys\": []}";
+            _env.Setup(x => x.Jwks).Returns(jwks);
+
+            _testObject.GetJwtConfig();
+
+            _jwkService.Verify(x => x.GetSigningKeys(jwks), Times.Once);
         }
     }
 }
