ea92c5ff5873f6b5a723762b08f89fd928524f07
Erik Muir <erikdmuir@gmail.com>
2020-07-07 10:02:11 -0500

    remove colored output text from build script

--- build.sh
+++ build.sh
@@ -1,15 +1,11 @@
 #! /bin/sh
 set -e
 
-RED='\e[0;31m'
-GREEN='\e[0;32m'
-NC='\e[m'
-
 version=$1
 
-[[ -z ${version} ]] && echo -e "${RED}Error: You must provide a version${NC}" && exit 1
+[[ -z ${version} ]] && echo "Error: You must provide a version" && exit 1
 re='^[0-9]+([.][0-9]+)*$'
-[[ ! ${version} =~ ${re} ]] && echo -e "${RED}Error: '${version}' is not a valid version string${NC}" && exit 1
+[[ ! ${version} =~ ${re} ]] && echo "Error: '${version}' is not a valid version string" && exit 1
 
 dotnet test
 dotnet tool update --global Amazon.Lambda.Tools
@@ -17,4 +13,4 @@ dotnet lambda package --project-location ./src/ApiGatewayCustomAuthorizer --msbu
 mkdir -p artifacts
 cp ./src/ApiGatewayCustomAuthorizer/bin/Release/netcoreapp3.1/ApiGatewayCustomAuthorizer.zip ./artifacts/custom-authorizer-${version}.zip
 
-echo -e "${GREEN}Successfully built and packaged version ${version}${NC}"
+echo "Successfully built and packaged version ${version}"
